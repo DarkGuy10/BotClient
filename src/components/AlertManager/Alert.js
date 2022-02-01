e9960ef9aaeb4b53812d62f881fc3b4fc805f427
DarkGuy10 <HarperBurn@protonmail.ch>
2022-02-01 21:19:23 +0900

    add: popping animation when alerts disappear

--- src/components/AlertManager/Alert.js
+++ src/components/AlertManager/Alert.js
@@ -3,27 +3,37 @@ import styles from './AlertManager.module.css'
 
 class Alert extends Component {
 	defaultTimeout = 4000
+	animationDuration = 100
 	constructor(props) {
 		super(props)
 		this.state = {
 			popped: false,
+			popping: false,
 		}
 	}
 
 	componentDidMount() {
 		const { popTimeout } = this.props
+		const timeout = popTimeout ?? this.defaultTimeout
 		setTimeout(() => {
-			this.setState({ popped: true })
-		}, popTimeout ?? this.defaultTimeout)
+			this.setState({ ...this.state, popping: true })
+		}, timeout - this.animationDuration)
+		setTimeout(() => {
+			this.setState({ ...this.state, popped: true })
+		}, timeout)
 	}
 
 	render() {
 		const { type, message } = this.props
-		const { popped } = this.state
+		const { popped, popping } = this.state
 		return (
 			<>
 				{popped ? null : (
-					<div className={`${styles.alertBox} ${styles[type]}`}>
+					<div
+						className={`${styles.alertBox} ${styles[type]} ${
+							popping ? styles.popping : ''
+						}`}
+					>
 						{message}
 					</div>
 				)}
