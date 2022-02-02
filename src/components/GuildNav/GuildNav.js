e56792db4163158773386946c2f83126fe60bd8a
DarkGuy10 <HarperBurn@protonmail.ch>
2022-02-02 23:25:09 +0900

    feat: add animation to guild icons on hover

--- src/components/GuildNav/GuildNav.js
+++ src/components/GuildNav/GuildNav.js
@@ -1,20 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import styles from './GuildNav.module.css'
 const { ipcRenderer } = window.require('electron')
 
 function ListItem(props) {
+	const [hover, updateHover] = useState(false)
 	return (
 		<div className={styles.listItem}>
 			<div className={styles.pill}>
-				<span className={props.selected ? styles.selected : ''}></span>
+				<span
+					className={
+						props.selected
+							? styles.selected
+							: hover
+							? styles.hover
+							: ''
+					}
+				></span>
 			</div>
 			<img
 				src={props.guild.iconURL}
 				alt={props.guild.name}
-				className={styles.icon}
+				className={`${styles.icon} ${
+					props.selected ? styles.iconSelected : ''
+				}`}
 				onClick={() => {
 					props.selectGuild(props.guild.id)
 				}}
+				onMouseEnter={() => updateHover(true)}
+				onMouseLeave={() => updateHover(false)}
 			/>
 		</div>
 	)
