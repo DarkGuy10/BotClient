fa3fe12436f6e34bb3eec1f8574b3776dd8c5744
DarkGuy10 <HarperBurn@protonmail.ch>
2022-02-07 15:38:15 +0900

    refactor(components): create `ToggleElement`
    This will be a reusable wrapper for ToggleButton

--- src/components/UserSettings/Settings/Storage.js
+++ src/components/UserSettings/Settings/Storage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AppData } from '../../../services'
-import { ToggleButton } from './../..'
+import { ToggleElement } from './../..'
 import styles from './Settings.module.css'
 
 const Storage = props => {
@@ -9,29 +9,20 @@ const Storage = props => {
 		<>
 			<div>
 				<h5 className={styles.title}>Token</h5>
-				<div className={styles.container}>
-					<div className={styles.labelRow}>
-						<label htmlFor="saveToken">
-							Persistent Login (save bot token in App Data).
-						</label>
-						<ToggleButton
-							onCheck={() => {
-								AppData.set('Storage.saveToken', true)
-								AppData.set('token', AppState.token)
-							}}
-							onUncheck={() => {
-								AppData.set('Storage.saveToken', false)
-								AppData.delete('token')
-							}}
-							defaultChecked={AppData.get(
-								'Storage.saveToken',
-								true
-							)}
-							id="saveToken"
-						/>
-					</div>
-					<div className={styles.divider}></div>
-				</div>
+
+				<ToggleElement
+					label="Persistent Login (save bot token in App Data)."
+					id="saveToken"
+					defaultChecked={AppData.get('Storage.saveToken', true)}
+					onCheck={() => {
+						AppData.set('Storage.saveToken', true)
+						AppData.set('token', AppState.token)
+					}}
+					onUncheck={() => {
+						AppData.set('Storage.saveToken', false)
+						AppData.delete('token')
+					}}
+				/>
 			</div>
 		</>
 	)
