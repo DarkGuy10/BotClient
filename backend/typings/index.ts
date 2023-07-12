28ec7bdcb4081e63f131a98ba3b91759ee67c9fb
DarkGuy10 <HarperBurn@protonmail.ch>
2023-07-12 12:46:24 +0530

    feat: add error messages

--- backend/typings/index.ts
+++ backend/typings/index.ts
@@ -18,5 +18,18 @@ export enum ClientErrorCodes {
 	INCORRECT_IPC_CHANNEL = 'INCORRECT_IPC_CHANNEL',
 	CANNOT_FETCH_USER = 'CANNOT_FETCH_USER',
 	CANNOT_CREATE_DM = 'CANNOT_CREATE_DM',
-	NO_SELECTED_CHANNEL = 'NO_SELECTED_CHANNEL',
+}
+
+export const ClientErrorMessages = {
+	CLIENT_NOT_READY: 'Discord client is not ready yet.',
+	NOT_GUILD_CHANNEL: 'Target channel is not a guild channel.',
+	NOT_TEXT_BASED_CHANNEL: 'Target channel is not a text-based channel.',
+	NO_CURRENT_CHANNEL: 'No channel is currently selected.',
+	CANNOT_FETCH_GUILD: 'Target guild could not be fetched.',
+	NO_VIEWABLE_CHANNEL_IN_GUILD: 'Target guild does not have a channel viewable by client user.',
+	CANNOT_FETCH_CHANNEL: 'Target channel could not be fetched.',
+	MISSING_PERMISSIONS: 'Client user does not have permissions to perform the action.',
+	INCORRECT_IPC_CHANNEL: 'Incorrect IPC channel was used.',
+	CANNOT_FETCH_USER: 'Target used could not be fetched.',
+	CANNOT_CREATE_DM: 'A DM channel could not be created with the target user.',
 }
