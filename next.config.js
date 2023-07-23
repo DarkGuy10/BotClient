6508a6c2fc3915f6f08392004178f387c485793b
DarkGuy10 <HarperBurn@protonmail.ch>
2023-07-23 19:16:18 +0900

    chore: enable HMR in dev mode

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ const { version, repository } = require('./package.json')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
-	trailingSlash: true,
 	distDir: 'build',
-	assetPrefix: '.',
+	output: 'export',
+	assetPrefix: process.env.NODE_ENV === 'production' ? '.' : undefined,
+	trailingSlash: true,
 	env: {
 		metaVersion: version,
 		metaRepositry: repository.url,
