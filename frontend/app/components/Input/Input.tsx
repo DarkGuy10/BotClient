7d5238c0d39d7c9c7258bf3b112ad5c8cec16fa5
DarkGuy10 <HarperBurn@protonmail.ch>
2023-07-29 13:55:20 +0530

    chore: WIP [skip ci] this commit will be resetted

---
+++ frontend/app/components/Input/Input.tsx
@@ -0,0 +1,1 @@
+import styles from './Input.module.scss'
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,24 +1,18 @@
-import styles from './page.module.css'
+import styles from '@/styles/dashboard.module.scss'
 
-export default function Home() {
+export default function Dashboard() {
 	return (
-		<div className={styles.wrapper}>
-			<main className={styles.main}>
-				<div className={styles.header}>
-					<h1 className={styles.headerText}>NextJS + Electron Boilerplate</h1>
-					<h4 className={styles.headerSubText}>
-						That one boilerplate you couldn{"'"}t find, until now.
-					</h4>
+		<div className={styles.layer}>
+			<div className={styles.wrapper}>
+				<div className={styles.addNewProfile}>
+					<h1 className={styles.header}>Add New Profile</h1>
+					<input
+						type="text"
+						className={styles.tokenField}
+						placeholder="Paste the token here..."
+					/>
 				</div>
-				<img className={styles.menhera} src="/menhera.png" alt="menhera" />
-				<div className={styles.note}>
-					<img src="/edit.svg" alt="edit" />
-					Get started by editting frontend/app/page.tsx
-				</div>
-				<footer className={styles.footer}>
-					{'<'}/{'>'} with ♥
-				</footer>
-			</main>
+			</div>
 		</div>
 	)
 }
