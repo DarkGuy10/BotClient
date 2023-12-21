7e5d9d6494383950c9b439bb97a3962e2ed39018
DarkGuy10 <62807269+DarkGuy10@users.noreply.github.com>
2023-12-21 21:22:33 +0530

    refactor: tweak alerts

--- frontend/lib/redux/features/alerts/AlertManager/AlertManager.tsx
+++ frontend/lib/redux/features/alerts/AlertManager/AlertManager.tsx
@@ -1,10 +1,11 @@
 'use client'
-import { useReduxDispatch, useReduxSelector } from '@/redux/hooks'
+import { useReduxSelector } from '@/redux/hooks'
 import styles from './AlertManager.module.scss'
 import { Alert } from '../Alert/Alert'
 
 export const AlertManager = () => {
 	const alerts = useReduxSelector(state => state.alerts.data)
+
 	return (
 		<div className={styles.container}>
 			{alerts.map(each => (
